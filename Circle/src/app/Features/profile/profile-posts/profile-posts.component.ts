fa725cab4ad65c3584943b20371956fc91aa46b4
Mirack <mirackchen@gmail.com>
2024-04-25 15:54:52 -0400

    init folder for navi and profile

---
+++ Circle/src/app/Features/profile/profile-posts/profile-posts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-posts',
+  templateUrl: './profile-posts.component.html',
+  styleUrls: ['./profile-posts.component.sass']
+})
+export class ProfilePostsComponent {
+
+}
