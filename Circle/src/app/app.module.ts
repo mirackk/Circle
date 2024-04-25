783a194bf137f50f24f3446d79f47e788cf209a2
Mirack <mirackchen@gmail.com>
2024-04-25 15:55:36 -0400

    share folder init

--- Circle/src/app/app.module.ts
+++ Circle/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './Shared/navigation/navigation.component';
+import { ProfileComponent } from './Features/profile/profile.component';
+import { ProfilePostsComponent } from './Features/profile/profile-posts/profile-posts.component';
+import { ProfileLikesComponent } from './Features/profile/profile-likes/profile-likes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    ProfileComponent,
+    ProfilePostsComponent,
+    ProfileLikesComponent
   ],
   imports: [
     BrowserModule,
