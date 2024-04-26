86b64c952c53bb329db12dba61fdec72e93480ee
mianzhiz123 <mianzhiz@usc.edu>
2024-04-26 14:01:07 -0700

    create feature 'home' & its children 'post' & 'story'

--- Circle/src/app/app.module.ts
+++ Circle/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Features/home/home.component';
+import { PostComponent } from './Features/home/post/post.component';
+import { StoryComponent } from './Features/home/story/story.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PostComponent,
+    StoryComponent
   ],
   imports: [
     BrowserModule,
