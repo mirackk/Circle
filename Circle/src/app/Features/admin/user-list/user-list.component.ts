19e78cd084528bf5a03a16de0d584da7571d5ec1
huozhixiang <zxhuous@gmail.com>
2024-04-28 14:22:30 -0500

    Implement Retrive Users

--- Circle/src/app/Features/admin/user-list/user-list.component.ts
+++ Circle/src/app/Features/admin/user-list/user-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../Shared/services/user.service';
+import { User } from './user.model';
 
 @Component({
   selector: 'app-user-list',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-list.component.sass']
 })
 export class UserListComponent {
+  users: User[] = [];
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getUserInfo().subscribe(data => {
+      this.users = data;
+      console.log(data);
+    }, error => {
+      console.error('There was an error!', error);
+    });
+  }
 
 }
